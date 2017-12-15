65adabdedf5390b1e9d13d49b5da6334727a9fe1
unknown <mmreyes13@gmail.com>
2017-12-14 23:55:15 -0500

    updates

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.get("/scrape", function(req, res) {
                 .create(result)
                 .then(function(dbArticle) {
                     // If we were able to successfully scrape and save an Article, send a message to the client
-                    res.send("Scrape Complete");
+                    res.send(dbArticle);
                 })
                 .catch(function(err) {
                     // If an error occurred, send it to the client
@@ -83,8 +83,9 @@ app.get("/articles", function(req, res) {
     db.Article
         .find({})
         .then(function(dbArticle) {
+            console.log(dbArticle);
             // If we were able to successfully find Articles, send them back to the client
-            res.json(dbArticle);
+            res.render("articles", { articles: dbArticle });
         })
         .catch(function(err) {
             // If an error occurred, send it to the client
